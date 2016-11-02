837cb9886035ca4405bfbb803c40f167e191a5e0
Vincent <v.labussiere@gmail.com>
2016-11-02 22:37:30 +0100

    Initialize sense-hat retro monsters

---
+++ Matrice/monster.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+def monster01(X,O):
+    return [
+    X, O, O, X, X, O, O, X,
+    X, O, X, X, X, X, O, X,
+    O, X, O, X, X, O, X, O,
+    O, X, X, X, X, X, X, O,
+    O, X, O, O, O, O, X, O,
+    O, O, X, X, X, X, O, O,
+    X, X, O, X, X, O, X, X,
+    X, O, O, O, O, O, O, X,
+    ]
+
+
+def monster02(X,O):
+    return [
+    O, X, O, O, O, O, X, O,
+    O, X, O, O, O, O, X, O,
+    O, O, X, O, O, X, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, O, X, X, O, X, O,
+    X, X, X, X, X, X, X, X,
+    O, X, X, O, O, X, X, O,
+    ]
+
+
+def monster03(X,O):
+    return [
+    O, O, O, X, X, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, X, X, X, X, X, O,
+    X, X, O, X, X, O, X, X,
+    X, X, X, X, X, X, X, X,
+    O, O, X, X, X, X, O, O,
+    O, X, X, X, X, X, X, O,
+    X, O, X, O, O, X, O, X,
+    ]
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from Matrice import monsters
+
+white=(255,255,255)
+black=(0,0,0)
+
+print('Test')
+print(monsters.monster01(white,black))
+
