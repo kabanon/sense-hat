e984fff932133c5e87e22231e4c5e4cc9d410391
Vincent <v.labussiere@gmail.com>
2016-11-03 22:25:12 +0100

    Monster have now their own class.

--- Matrice/monsters.py
+++ Matrice/monsters.py
@@ -1,40 +1,70 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-def monster01(X,O):
-    return [
-    X, O, O, X, X, O, O, X,
-    X, O, X, X, X, X, O, X,
-    O, X, O, X, X, O, X, O,
-    O, X, X, X, X, X, X, O,
-    O, X, O, O, O, O, X, O,
-    O, O, X, X, X, X, O, O,
-    X, X, O, X, X, O, X, X,
-    X, O, O, O, O, O, O, X,
-    ]
+class Monter:
+  X = (255,255,255)
+  O = (0,0,0)
+  
+  monsters = {
+    '01' : [
+      X, O, O, X, X, O, O, X,
+      X, O, X, X, X, X, O, X,
+      O, X, O, X, X, O, X, O,
+      O, X, X, X, X, X, X, O,
+      O, X, O, O, O, O, X, O,
+      O, O, X, X, X, X, O, O,
+      X, X, O, X, X, O, X, X,
+      X, O, O, O, O, O, O, X, 
+    ],
+    '02' : [
+      O, X, O, O, O, O, X, O,
+      O, X, O, O, O, O, X, O,
+      O, O, X, O, O, X, O, O,
+      O, O, O, X, X, O, O, O,
+      O, O, X, X, X, X, O, O,
+      O, X, O, X, X, O, X, O,
+      X, X, X, X, X, X, X, X,
+      O, X, X, O, O, X, X, O,
+    ],
+    '03' : [
+      O, O, O, X, X, O, O, O,
+      O, O, X, X, X, X, O, O,
+      O, X, X, X, X, X, X, O,
+      X, X, O, X, X, O, X, X,
+      X, X, X, X, X, X, X, X,
+      O, O, X, X, X, X, O, O,
+      O, X, X, X, X, X, X, O,
+      X, O, X, O, O, X, O, X,
+    ],
+  }
+
+  def __init__(self, sense):
+    self.monster = None
+    self.action  = None
+    self.sense   = sense
+
+  def load(self, monster_id):
+    #if (self.monsters.has_key(monster_id)):
+    self.monster = self.monsters[monster_id]
+
+  def display(self):
+    if (self.monster is None):
+      self.sense.set_pixels(self.monster)
 
 
-def monster02(X,O):
-    return [
-    O, X, O, O, O, O, X, O,
-    O, X, O, O, O, O, X, O,
-    O, O, X, O, O, X, O, O,
-    O, O, O, X, X, O, O, O,
-    O, O, X, X, X, X, O, O,
-    O, X, O, X, X, O, X, O,
-    X, X, X, X, X, X, X, X,
-    O, X, X, O, O, X, X, O,
-    ]
+  def __str__(self):
+    if (self.monster is None):
+      return 'No monster loaded'
+    else:
+      _return = ""
+      for i in range(0,64,8):
+        for j in range(0,8):
+          if (self.monster[i+j] == self.X):
+            _return += 'X'
+          else:
+            _return += ' '
+        _return += "\n"
 
+      return _return
 
-def monster03(X,O):
-    return [
-    O, O, O, X, X, O, O, O,
-    O, O, X, X, X, X, O, O,
-    O, X, X, X, X, X, X, O,
-    X, X, O, X, X, O, X, X,
-    X, X, X, X, X, X, X, X,
-    O, O, X, X, X, X, O, O,
-    O, X, X, X, X, X, X, O,
-    X, O, X, O, O, X, O, X,
-    ]
+       
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
+import time
+from sense_hat import SenseHat
 from Matrice import monsters
 
-white=(255,255,255)
-black=(0,0,0)
+sense = SenseHat()
+sense.clear()
+sense.low_light = True
 
-print('Test')
-print(monsters.monster01(white,black))
+monster = monsters.Monter(sense)
+monster.load('01')
+sense.set_pixels(monster.monster)
+print(monster)
+time.sleep(2)
+monster.load('02')
+sense.set_pixels(monster.monster)
+print(monster)
+time.sleep(2)
+monster.load('03')
+sense.set_pixels(monster.monster)
+print(monster)
+time.sleep(2)
 
+sense.clear()
