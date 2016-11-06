83cfddf610f822389b559c08521755d1f966d64f
Vincent <v.labussiere@gmail.com>
2016-11-06 16:38:57 +0100

    Monsters can now move

--- Matrice/monsters.py
+++ Matrice/monsters.py
@@ -4,16 +4,21 @@
 import json
 import os
 
-class Monter:
+class Monter():
   
   def __init__(self):
     self.monster = None
-    self.action  = None
     self.colors = {
       0 : (0,0,0),
       1 : (255,255,255)
     }
-    self.position = [0,0]
+    self.position = {
+      'x':0,
+      'y':0,
+    }
+    self.step = 8
+    self.size = self.step * self.step
+
 
   def set_X(self, X):
     self.X = X
@@ -28,14 +33,23 @@ class Monter:
         print("Error: File '{}' does not appear to exist.".format(filename))
 
   def display(self):
+    _return = list(range(0, self.size))
     # Only return data if monster is defined.
     if (self.monster is not None):
-      # Take care of monster position in the matrice.
-      _return = list(range(0,64))
-      for i in range(0,64,8):
-        for j in range(0,8):
-          _return[i+j] = self.colors.get(self.monster[i+j], (0,0,0))
-      return _return
+      for y in range(0, self.size, self.step):
+        for x in range(0, self.step):
+          _pos = y+x
+          # Define a default value. 
+          _return[_pos] = self.colors[0]
+          # Check if x position is in visible area
+          _check_x = x - self.position['x']
+          if (_check_x >= 0 and _check_x < self.step):
+            # Check if y position is in visible area
+            _check_y = int(y / self.step - self.position['y']) * self.step
+            if (_check_y >= 0 and _check_y < self.size):
+              # Get pixel to display 
+              _return[_pos] = self.colors.get(self.monster[_check_x+_check_y], self.colors[0])
+    return _return
 
   def __str__(self):
     if (self.monster is None):
@@ -44,7 +58,7 @@ class Monter:
       _return = ""
       for i in range(0,64,8):
         for j in range(0,8):
-          if (self.monster[i+j] == 1):
+          if (self.monster[i+j] >= 1):
             _return += 'X'
           else:
             _return += ' '
